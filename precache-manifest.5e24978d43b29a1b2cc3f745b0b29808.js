ab9821a05d868864576f879f4d82643165768004
IncredibLink <link@langchao.org>
2020-02-29 20:09:49 -0500

    Updates

---
+++ precache-manifest.5e24978d43b29a1b2cc3f745b0b29808.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da2feee6ecc4b27e846c5f9d3e7b314e",
+    "url": "/article-annotator/index.html"
+  },
+  {
+    "revision": "be51bf320e382bb3b490",
+    "url": "/article-annotator/static/css/main.16a0a965.chunk.css"
+  },
+  {
+    "revision": "211e1cb919f27407822a",
+    "url": "/article-annotator/static/js/2.ed371436.chunk.js"
+  },
+  {
+    "revision": "41b8e85ea6872af67320e33672b46840",
+    "url": "/article-annotator/static/js/2.ed371436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be51bf320e382bb3b490",
+    "url": "/article-annotator/static/js/main.29b250f9.chunk.js"
+  },
+  {
+    "revision": "067ce25c82bb6bb5599c",
+    "url": "/article-annotator/static/js/runtime-main.e46c8248.js"
+  }
+]);
